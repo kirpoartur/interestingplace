2c9f1191408d96cab81ee006fe3d75ea96cfbaac
ёжз <40928593+ezhzz@users.noreply.github.com>
2021-06-02 20:28:31 +0700

    Add files via upload

--- public/create.js
+++ public/create.js
@@ -3,16 +3,20 @@ let butt = document.getElementById('bt')
 function create() {
   text = document.getElementById('at').value
   name = document.getElementById('name').value
-  name = encodeURIComponent(name)
+  if (text != '' && text != ' ' && name != '' && name != ' ') {
+    name = encodeURIComponent(name)
 
-  filename = String(Math.random() * 10000000000000000)
-  var xmlHttp = new XMLHttpRequest()
-  xmlHttp.open("GET", '/withlovefosssru/' + filename + "/" + encodeURIComponent(text) + '/' + name, false)
-  console.log(name)
-  xmlHttp.addEventListener("load", function (){
-    window.location.href = '/articles/' + filename + '.html'
-  })
-  xmlHttp.send(null)
+    filename = String(Math.random() * 10000000000000000)
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.open("GET", '/withlovefosssru/' + filename + "/" + encodeURIComponent(text) + '/' + name, false)
+    console.log(name)
+    xmlHttp.addEventListener("load", function (){
+      window.location.href = '/articles/' + filename + '.html'
+    })
+    xmlHttp.send(null)
+  } else {
+    alert('Имя и текст поста не могут быть пустыми :|')
+  }
 }
 
 butt.addEventListener('click', create)
